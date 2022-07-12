470ffb62752880dc4a400f67393a850120ee8f76
Iris <turba@cip.ifi.lmu.de>
2022-07-12 11:35:54 +0200

    crawl wiki section and show als resolved text

--- chrome-ex/src/components/Search.js
+++ chrome-ex/src/components/Search.js
@@ -4,6 +4,7 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {getCurrentTab} from "../Utils";
 import TrafficContainer from "./TrafficContainer"
+var parse = require('html-react-parser');
 
 
 const Search = () => { 
@@ -11,7 +12,12 @@ const Search = () => {
     const [term, setTerm] = useState("React")
     const [results, setResults] = useState([])
     const [seeAlso, setSeeAlso] = useState([])
+    const [links, setLinks] = useState([])
     const [pageContent, setPageContent] = useState('N/A');
+    const [language, setLanguage] = React.useState('en');
+    const [sectionNum, setSectionNum] = useState(0);
+
+    var section = 0; //variable for the see also section
 
     useEffect(() =>{
         chrome.storage.onChanged.addListener(function (changes,areaName) {
@@ -43,38 +49,73 @@ const Search = () => {
                     });
         });
     });
-
-       /* chrome.scripting.executeScript({
-            target: {tabId: tabId},
-            func: getText,
-            
-        }, function(results) {
-            // results.length must be 1
-            var result = results[0];
-            setPageContent(result);
-            console.log(result);
-        });
-        */
-      /*  chrome.tabs.query({
-            active: true, 
-            lastFocusedWindow:true
-        },function(tabs) { 
-            chrome.tabs.sendMessage(
-                tabs[0].id,
-                {from: 'app', subject: 'getText'}, function(response) {
-                if(response.method=="getText"){
-                    setPageContent(response.data);
-                    console.log(pageContent);
-                }
-            });
-        });*/
     });
+
+    //search "see also" e.g. https://en.wikipedia.org/w/api.php?action=parse&page=Pune&format=json&prop=sections
+    // and then https://en.wikipedia.org/w/api.php?action=parse&page=Pune&format=json&section=42
+    useEffect(() => {
+        //search Wikipedia API
+        
+        const searchSA = async () => {
+            const { data } = await axios.get(`https://${language}.wikipedia.org/w/api.php`, {
+                params: {
+                    action: "parse",
+                    prop: "sections",
+                    format: "json",
+                    origin: "*",
+                    page: term,
+                },
+            })
+            console.log(data);
+            const sections = data.parse.sections;
+            console.log(data.parse.sections)
+            
+            for(var i=0; i < sections.length; i++){
+                if(sections[i].line == 'See also'){
+                    console.log(sections[i]);
+                    console.log(i);
+                    section = i;
+                }
+            }}
+            console.log(section);
+            const searchSA2 = async () => {
+            const { data } = await axios.get(`https://${language}.wikipedia.org/w/api.php`, {
+                params: {
+                    action: "parse",
+                    prop: "text",
+                    format: "json",
+                    origin: "*",
+                    page: term,
+                    section: 8,
+                },
+            })
+            console.log(data);
+            console.log(data.parse.text["*"]);
+            setSeeAlso(parse(`<div>${data.parse.text["*"]}</div>`));            
+        }
+        
+        if (term && !seeAlso.length){
+            searchSA();
+            searchSA2();
+        }else{
+        let timeoutID = setTimeout(() =>{
+        if(term){
+        searchSA()
+        searchSA2();
+        }
+    },1000);
+    return () =>{
+        clearTimeout(timeoutID);
+    }
+}
+    }, [term])
+   
 
     //search links
     useEffect(() => {
         //search Wikipedia API
-        const searchSA = async () => {
-            const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        const searchLinks = async () => {
+            const { data } = await axios.get(`https://${language}.wikipedia.org/w/api.php`, {
                 params: {
                     action: "query",
                     prop: "links",
@@ -83,18 +124,18 @@ const Search = () => {
                     titles: term,
                 },
             })
-            console.log(data);
+            //console.log(data);
             const keys = Object.keys(data.query.pages)
-            console.log(data.query.pages[keys[0]].links)
-            setSeeAlso(data.query.pages[keys[0]].links)
+            //console.log(data.query.pages[keys[0]].links)
+            setLinks(data.query.pages[keys[0]].links)
             
         }
         if (term && !seeAlso.length){
-            searchSA();
+            searchLinks();
         }else{
         let timeoutID = setTimeout(() =>{
         if(term){
-        searchSA()
+        searchLinks()
         }
     },1000);
     return () =>{
@@ -107,7 +148,7 @@ const Search = () => {
     useEffect(() => {
         //search Wikipedia API
         const search = async () => {
-            const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+            const { data } = await axios.get(`https://${language}.wikipedia.org/w/api.php`, {
                 params: {
                     action: "query",
                     list: "search",
@@ -133,14 +174,13 @@ const Search = () => {
 }
     }, [term])
 
-   
 
     const searchResultsMapped = results.map(result =>{
         return(
             <div className="item" key={result.pageid}>
                 <div className="content">
                     <h3 className="header">{result.title}</h3>
-                    <span className='link'><a href={`https://en.wikipedia.org/wiki/${result.title}`}>{`https://en.wikipedia.org/wiki/${result.title}`}</a></span><br/>
+                    <span className='link'><a href={`https://${language}.wikipedia.org/wiki/${result.title}`}>{`https://${language}.wikipedia.org/wiki/${result.title}`}</a></span><br/>
                     <span dangerouslySetInnerHTML={{__html:result.snippet}}></span>
 
                 </div>
@@ -148,28 +188,44 @@ const Search = () => {
         )
     })
 
-    const linksInArticle = seeAlso.map(link =>{
+    const linksInArticle = links.map(link =>{
         return (
             <div>
-            <span className='link'><a href={`https://en.wikipedia.org/wiki/${link.title}`}>{link.title}</a></span><br/>   
+            <span className='link'><a href={`https://${language}.wikipedia.org/wiki/${link.title}`}>{link.title}</a></span><br/>   
             </div>
         )
     })
 
+    const handleLanguage = (event) => {
+    setLanguage(event.target.value);
+    };
     
 
     return(
       <div>
           <div className="ui-form">
               <div className="field">
+                  <div><label>
+                        Select language
+                        <select value={language} onChange={handleLanguage}>
+                        <option value="en">English</option>
+                        <option value="de">German</option>
+                        <option value="fr">French</option>
+                        <option value="it">Italian</option>
+                        </select>
+                  </label></div>
                   <label>Search Term</label>
+                  
                   <input className="input"
                   value={term}
                   onChange={e => setTerm(e.target.value)}
                   />
               </div>
           </div>
+          <p>First 10 Links:</p>
           <div>{linksInArticle}</div>
+          <p>See also:</p>
+          <div>{seeAlso}</div>
           <div className="ui celled list">{searchResultsMapped}</div>
       </div>
       )
