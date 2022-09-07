e0b0e63b1ebc11cb6fdc9d839d188b97075c6f0a
Iris <turba@cip.ifi.lmu.de>
2022-09-07 17:27:19 +0200

    trying to fix search

--- chrome-ex/src/components/Dictionary.js
+++ chrome-ex/src/components/Dictionary.js
@@ -1,7 +1,8 @@
 /* global chrome */
 import React, {useState, useContext, useEffect, useRef} from 'react';
-import Card from "./Card";
+import IndexCard from "./IndexCard";
 import BasicWikiCard from '../BasicWikiCard';
+import Card from '@mui/material/Card';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faCaretLeft,
@@ -56,65 +57,9 @@ function Dictionary (){
             }).catch((error) => {
             error.toString();
         })
-    }, [value])
+    })
 
 
-   /* useEffect(() =>{
-        const sDict = localStorage.getItem("Vocabulary");
-        const parsedDict = JSON.parse(sDict);
-
-        if(sDict == null){
-            setValue([])
-        }else{
-            setValue(parsedDict)
-        }
-    })*/
-
-    useEffect(() =>{
-      /*  if(localStorage.getItem("Vocabulary") ){
-            setValue(JSON.parse(localStorage.getItem("Vocabulary")));
-            console.log(JSON.parse(localStorage.getItem("Vocabulary")));
-        }*/
-
-      /*  if(value !==  []){
-            localStorage.setItem("Vocabulary", JSON.stringify(value));
-            console.log(localStorage.getItem("Vocabulary"));
-            //chrome.storage.sync.set({dictionary: {Term: value.Term, Translation: value.Translation}})
-        }else if(value == []){
-            setValue(JSON.parse(localStorage.getItem("Vocabulary")));
-            console.log(JSON.parse(localStorage.getItem("Vocabulary")));
-        }*/
-        
-    },[value])
-
-    /**
-     * add new word to Chrome Storage and update dictionary
-     */
-  /*  useEffect (() =>{
-    chrome.storage.sync.set({dictionary: newWord}, function(){    
-            console.log(newWord + "added to chrome storage");
-    });
-    chrome.storage.sync.get({
-        dictionary:[] 
-    },
-    function(data) {
-       console.log(data.dictionary);
-       update(data.dictionary); //storing the storage value in a variable and passing to update function     
-    }); 
-    }, [fullDictionary])
-
-
-    function update(array)
-   {
-    array.push("test");
-    //then call the set to update with modified value
-    chrome.storage.sync.set({
-        Dictionary:array
-    }, function() {
-        setFullDictionary(array);
-        console.log(fullDictionary);
-    });
-    }*/
 
     const slideLeft = () => {
         if (index - 1 >= 0) {
@@ -149,6 +94,7 @@ function Dictionary (){
         console.log(filteredVoc)
         setFullDictionary(value);
         setValue(filteredVoc)
+        console.log(value);
        // if(filteredVoc){
         //sendLog("Searched for Term " + searchField, filteredVoc[0].term, filteredVoc[0].translation, filteredVoc[0].motherTounge, filteredVoc[0].targetlanguage)}
     }
@@ -243,6 +189,7 @@ function Dictionary (){
     const words =     
     value && value.map((value,index) =>{
         return(
+            <Card  className="cardcontainer" >
             <div onClick={() => {handlechange(index);}} key={index} className="container">
             <h3>{value.term}</h3>
             <a target="_blank" href={`https://${value.targetlanguage}.wikipedia.org/wiki/${value.link}`}>{value.translation}</a>
@@ -254,9 +201,9 @@ function Dictionary (){
             size="2x"
             color="#B2BFC7"
              />
-            <hr class="solidHR"></hr>
             </div>
             </div>
+            </Card>
             
             
         )
@@ -268,12 +215,12 @@ function Dictionary (){
         let position = n > index ? "nextCard" : n === index ? "activeDictCard" : "prevCard";
         return(
         <div onClick={() => {handlechange(n);}} className="container" key={n}>
-            <Card {...value} cardStyle={position}></Card>
+            <IndexCard {...value} cardStyle={position}></IndexCard>
         </div>
     )
     })
 
-    const filteredVoc = value && value.filter(
+    const filteredVoc = value.filter(
         value =>{
             return(
                 value.term?.toLowerCase().includes(searchField.toLowerCase()) ||
@@ -285,7 +232,7 @@ function Dictionary (){
 
     return(
         <div>
-            {(value?.length>0) ?  
+            {value !=null ?  
         <div>
             <div><input
              type="search"
--- chrome-ex/src/components/IndexCard.js
+++ chrome-ex/src/components/IndexCard.js
@@ -1,11 +1,16 @@
-const Card = ({ handleMouseDown, term, translation, targetlanguage, link, cardStyle }) => {
+import Card from '@mui/material/Card';
+
+const IndexCard = ({ handleMouseDown, term, translation, targetlanguage, link, cardStyle }) => {
+
   
     return (
+      <Card>
       <div className={cardStyle} onMouseDown={handleMouseDown}>
             <h3>{term}</h3>
             <a target="_blank" href={`https://${targetlanguage}.wikipedia.org/wiki/${link}`}>{translation}</a>
       </div>
+      </Card>
     );
   };
   
-  export default Card;
+  export default IndexCard;
